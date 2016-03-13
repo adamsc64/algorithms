5f77aea8f132bdbb36725a00c3817cea1e3c7f64
Christopher Adams <christopher.r.adams@gmail.com>
2016-03-12 19:53:12 -0500

    Create the bitmap, init to all 0

--- bloom/bloom_filter.py
+++ bloom/bloom_filter.py
@@ -2,6 +2,7 @@ class BloomFilter(object):
 
     def __init__(self, num_bits):
         self.num_bits = num_bits
+        self.bits = [0] * num_bits
 
     def __repr__(self):
         return u"BloomFilter(num_bits=%s)" % self.num_bits
