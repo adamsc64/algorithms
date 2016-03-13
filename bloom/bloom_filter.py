7e1ca4d025208238786335d6860328344d5a1b0e
Christopher Adams <christopher.r.adams@gmail.com>
2016-03-12 20:13:47 -0500

    Add way to specify number of hashers

--- bloom/bloom_filter.py
+++ bloom/bloom_filter.py
@@ -1,8 +1,19 @@
 class BloomFilter(object):
 
-    def __init__(self, num_bits):
+    def __init__(self, num_bits, num_hashers):
         self.num_bits = num_bits
+        self.num_hashers = num_hashers
         self.bits = [0] * num_bits
+        self.hashers = [get_hasher() for i in range(num_hashers)]
 
     def __repr__(self):
-        return u"BloomFilter(num_bits=%s)" % self.num_bits
+        return u"BloomFilter(num_bits={0}, num_hashers={1})".format(
+            self.num_bits,
+            self.num_hashers,
+        )
+
+
+def get_hasher():
+    def hasher(target):
+        pass
+    return hasher
