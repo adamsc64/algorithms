ad9a98eabf83b98b413187351c35d6caa086946f
Christopher Adams <christopher.r.adams@gmail.com>
2016-03-12 20:26:47 -0500

    Made hash generator a class method

--- bloom/bloom_filter.py
+++ bloom/bloom_filter.py
@@ -4,7 +4,7 @@ class BloomFilter(object):
         self.num_bits = num_bits
         self.num_hashers = num_hashers
         self.bits = [False] * num_bits
-        self.hashers = [get_hasher() for i in range(num_hashers)]
+        self.hashers = [self.get_hasher() for i in range(num_hashers)]
 
     def __repr__(self):
         return u"BloomFilter(num_bits={0}, num_hashers={1})".format(
@@ -12,8 +12,7 @@ class BloomFilter(object):
             self.num_hashers,
         )
 
-
-def get_hasher():
-    def hasher(target):
-        pass
-    return hasher
+    def get_hasher(self):
+        def hasher(target):
+            pass
+        return hasher
